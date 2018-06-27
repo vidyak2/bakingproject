4963190bb10328fe2e4f5c0e86c298dbb216f303
vidyak2 <vidyakrishnan.vk@gmail.com>
2018-06-27 14:49:19 -0700

    Correct comments

--- javascript/script.js
+++ javascript/script.js
@@ -1,23 +1,25 @@
+//Create html from js data
+
 var muffins = [
   {
     name: "Chocolate Muffins with Vanilla Frosting",
-    p: "Baking chococolate muffins ... the recipe that got me interested in baking. This recipe is very simple and easy. Give it a try before getting into the more complicate items. Suggest baker level: Begginer",
+    p: "Chococolate muffins ... the recipe that got me interested in baking. This recipe is very simple. Give it a try before getting into the more complicate items. Suggest baker level: Begginer",
     image: "images/chocmuffins.png"
   },
   {
     name: "Vanilla Cupcakes with Chocolate Frosting",
-    p: "Baking vanilla cupcakes is easy, considering that you re using the right recipe, and respect the baking time. Be aware, vanilla cupcakes can easily harden if left too long in the oven. Suggested baker level: Begginer",
+    p: "Baking vanilla cupcakes is easy, considering that you are using the right recipe, and respect the baking time. Be aware, vanilla cupcakes can easily harden if left too long in the oven. Suggested baker level: Begginer",
     image: "images/vanillacupcake.JPG"
   },
   {
-    name: "Cake Inside a Cupcake",
-    p: "A great gift to your loved ones on Valentine's Day...or any day! Baking a cake inside a cupcake is a process that demands patience and time. Using two of your favorite cake recipes, and a cookie cutter, these cupcakes can be done and enjoyed by all. Suggested baker level: Intermediate",
-    image: "images/cakewithin.JPG"
+    name: "Banana & Chocolate Chip Muffins",
+    p: "Have few extra bananas at home? This muffin recipe is very simple and quick. Not to mention, there isn't much sugar involved; you can almost pass it as a healthy snack! Suggested baker level: Begginer",
+    image: "images/banana.jpg"
   },
   {
-    name: "Banana & Chocolate Chip Muffins",
-    p: "Have extra few extra bananas at home? This muffin recipe is very simple and quick. Not to mention, there isn't much sugar in this recipe, and you can pass it as almost healthy! Suggested baker level: Begginer",
-    image: "images/banana.jpg"
+    name: "Cake Inside a Cupcake",
+    p: "A great gift to your loved ones on Valentine's Day...or any day! Baking a cake inside a cupcake is a process that demands patience and time. Using two of your favorite cake recipes, and a cookie cutter, these cupcakes can be created and enjoyed by all. Suggested baker level: Intermediate",
+    image: "images/cakewithin.JPG"
   }
 ]
 
@@ -28,7 +30,7 @@ var pies = [
     image: "images/pie.png"
 }, {
     name: "Berry Pie with Twist Top",
-    p: "Baking a berry pie with a twist top is identical to a regular pie, although the top pie crust is cut into strips and twirled to create this design. Suggested baker level: Begginer",
+    p: "A berry pie with a twist top is identical to a regular pie, although the top pie crust is cut into strips and twirled to create this design. Suggested baker level: Begginer",
     image: "images/berrypie.jpg"
 }
 ]
@@ -36,36 +38,37 @@ var pies = [
 var cakes = [
   {
     name: "Applesauce Brownies",
-    p: "Craving extreme chocolate? This simple brownie recipe uses applesauce and no eggs to create exceptionally tasty and chocolaty brownies. Suggested baker level: Begginer",
+    p: "Craving extreme chocolate? This simple brownie uses applesauce and no eggs to create exceptionally tasty and chocolaty brownies. Suggested baker level: Begginer",
     image: "images/applesaucebrownie.jpg"
   },
   {
     name: "Mini Chocolate Bundt Cakes",
-    p: "Baking bundt cakes can be tricky, because the consistency may vary depending on the pan. This is my very first bundt cake recipe, made with the same recipe as the chocolate muffins. Suggested baker level: Begginer",
+    p: "Baking bundt cakes can be tricky, because the consistency may vary depending on the pan. This is my very first bundt cake trial, and was made with the same recipe as the chocolate muffins. Suggested baker level: Begginer",
     image: "images/minibundt.JPG"
   },
   {
-    name: "Chocolate and Vanille Layer Cookie Cake",
+    name: "Chocolate and Vanila Layer Cookie Cake",
     p: "Baking this layer cake is more difficult than it seems, since the base vailla layer is lightly cooked before adding the chocolate batter and cooking the cake again. Suggested baker level: Intermediate",
     image: "images/chocvanillacake.JPG"
   },
   {
     name: "Lemon Bundt Cake with Vanilla Icing",
-    p: "This lemon cake gives a great summer feel and is simple to make. The extra challenge is to use lemon zest and really bring out the flavors. Suggested baker level: Intermediate",
+    p: "This lemon cake gives a great summer feel. The challenge is to use lemon zest and really bring out the flavors. Suggested baker level: Intermediate",
     image: "images/lemoncake.jpg"
   },
   {
     name: "Mini Confetti Bundt Cakes",
-    p: "Have a birthday party to go to? This recipe creates fun little cakes for everyone. This recipe is simple, although it uses several eggs, and a lot of patience. Suggested baker level: Intermediate",
+    p: "Have a birthday party to go to? Confetti cakes are fun for everyone. The recipe is simple, although it uses several eggs. Suggested baker level: Intermediate",
     image: "images/confetti.jpg"
   },
   {
     name: "Multicolor Heart Cakes",
-    p: "Valentine's day again, and you made the Cake Inside a Cake last year? No worries, this year you can bake these beautiful heart shaped cakes. The recipe is the same as your favorite cake, however, the batter must be seperated to created the different colors, and piped one after the other. Suggested baker level: Intermediate",
+    p: "Valentine's day again and you made the Cake Inside a Cake last year? No worries! This year, you can bake these beautiful heart shaped cakes. The recipe is the same as your favorite cake, however, the batter must be seperated to create different colors, and piped one after the other. Suggested baker level: Intermediate",
     image: "images/multicolorcake.jpg"
   }
 ]
 
+//Create muffin list
 function muffinList() {
 
   // Select element with id "target"
@@ -76,10 +79,12 @@ function muffinList() {
   //Create product listing from a database
   for (var i = 0; i < muffins.length; i++) {
 
+    //Create div element
     var div = document.createElement("div")
     //Add class to add styles
     div.classList.add("itemdiv")
 
+    //Create elements within the div
     var h2 = document.createElement("h2")
     h2.innerHTML = muffins[i].name
     h2.classList.add("itemh2")
@@ -101,6 +106,7 @@ function muffinList() {
   }
 }
 
+//Call function
 muffinList()
 
 function pieList() {
